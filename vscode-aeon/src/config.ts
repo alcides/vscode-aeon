f4ad49b7f18c9bf79cc8501e5678172501858c11
Diogo Sousa <sousanamain@gmail.com>
2025-07-10 14:49:02 +0100

    add a way to use a boolean flag to signal the use of a system interpreter (with the path 'aeon')

--- vscode-aeon/src/config.ts
+++ vscode-aeon/src/config.ts
@@ -3,10 +3,17 @@ import { workspace } from 'vscode'
 import * as path from 'node:path'
 
 export function envPath(context: vscode.ExtensionContext): string {
-    const configInterpreterPath: string | undefined = vscode.workspace.getConfiguration('aeon').get('interpreterPath')
-    if (configInterpreterPath) {
+    const configInterpreterPath: string | undefined = vscode.workspace.getConfiguration('aeon').get('environmentPath')
+    if (configInterpreterPath && configInterpreterPath.trim() !== '') {
         return configInterpreterPath
     }
 
     return path.join(context.globalStorageUri.fsPath, 'interpreter')
+}
+
+export function useSystemInterpreter(): boolean {
+    const useSystemInterpreter: boolean | undefined = vscode.workspace.getConfiguration('aeon')
+        .get('useSystemInterpreter')
+
+    return useSystemInterpreter === true
 }
